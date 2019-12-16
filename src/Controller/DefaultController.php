72ef53d568092fade2b606cc5ceab7d9611e2d09
famonsei <fabrice.monseigne@gmail.com>
2019-12-16 17:30:50 +0100

    feat(command): Move log file in order to include it into a twig render And add firewalls

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,15 +13,39 @@ class DefaultController extends AbstractController
      */
     public function testsResult()
     {
-        $title = 'No result tests yet';
+        $title = 'No tests result yet';
 
         $testsResultFile = dirname(__DIR__) . '/../templates/tests/test.all.html';
         if (file_exists($testsResultFile)) {
-            $title =  'Result tests at ' . date ('D jS Y H:i:s', filemtime($testsResultFile));
+            $title =  'Tests result at ' . date ('D jS Y H:i:s', filemtime($testsResultFile));
         }
 
         return $this->render('tests/result.html.twig', [
             'title' => $title
         ]);
+    }
+
+    /**
+     * @Route("/sync", name="sync_result")
+     */
+    public function syncResult()
+    {
+        $title = 'No sync result yet';
+
+        $syncResultFile = dirname(__DIR__) . '/../templates/sync/' . $_ENV['APP_ENV'] . '.sync.log';
+        if (file_exists($syncResultFile)) {
+            $title =  'Sync result at ' . date ('D jS Y H:i:s', filemtime($syncResultFile));
+        }
+
+        return $this->render('sync/result.html.twig', [
+            'title' => $title
+        ]);
+    }
+
+    /**
+     * @Route("/ldap", name="ldap")
+     */
+    public function ldap() {
+        return new Response("LOL",Response::HTTP_OK);
     }
 }
